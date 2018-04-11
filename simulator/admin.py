f3b6913c414cdb28cdc120e5b1b3a6f18b91ae21
luigidino3 <jose_luis_dino@dlsu.edu.ph>
2018-04-11 21:41:19 +0800

    admin.py

--- simulator/admin.py
+++ simulator/admin.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.contrib import admin
-from .models import Register, DataSegment, MipsProgram, MemoryClearer, IF, ID, EX, MEM, WB
+from .models import Register, DataSegment, MipsProgram, MemoryClearer, IF, ID, EX, MEM, WB,Table
 
 admin.site.register(Register)
 admin.site.register(DataSegment)
@@ -12,3 +12,4 @@ admin.site.register(ID)
 admin.site.register(EX)
 admin.site.register(MEM)
 admin.site.register(WB)
+admin.site.register(Table)
